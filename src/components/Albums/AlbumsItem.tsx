81ee85146c31191f84f91ac753a303e230fb6b4e
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-01-15 20:44:05 +0100

    Photos to albums added

--- src/components/Albums/AlbumsItem.tsx
+++ src/components/Albums/AlbumsItem.tsx
@@ -1,5 +1,8 @@
 import { Link, useSubmit } from "react-router-dom";
 import classes from "./AlbumItem.module.css";
+import PhotosList from "../Photos/PhotosList";
+import { useState } from "react";
+import PhotoForm from "../Photos/PhotoForm";
 
 interface Albums {
     userId: number;
@@ -12,7 +15,7 @@ interface Props {
 }
 
 const AlbumsItem: React.FC<Props> = ({ albums }) => {
-  console.log("AlbumsItem", albums);
+  const [addPhoto, setAddPhoto] = useState(false);
   const submit = useSubmit();
 
   function startDeleteHandler(){
@@ -23,14 +26,26 @@ const AlbumsItem: React.FC<Props> = ({ albums }) => {
     }
   }
 
+  function addPhotoHandler(){
+    setAddPhoto(!addPhoto);
+  }
+
   return (
+    <>
     <article className={classes.albums}>
       <h1>{albums.title}</h1>
       <menu className={classes.actions}>
         <Link to={`/albums/${albums.id}/edit`}>Edit</Link>
         <button onClick={startDeleteHandler}>Delete</button>
+        <button onClick={addPhotoHandler}>Photo</button>
       </menu>
     </article>
+    {addPhoto ? (
+      <PhotoForm albumId={albums.id} />
+    ) : (
+      <PhotosList albumId={albums.id} />
+    )}
+    </>
   );
-}
+};
 export default AlbumsItem;
